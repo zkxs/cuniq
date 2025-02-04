45237d8cd2f7faa92b1b5fe09fdab8c9aefc79a4
Michael Ripley <zkxs00@gmail.com>
2025-02-03 18:48:17 -0600

    bump copyright years

--- line_cardinality/benches/hashtable_no_fn.rs
+++ line_cardinality/benches/hashtable_no_fn.rs
@@ -1,4 +1,4 @@
-// This file is part of line_cardinality. Copyright © 2024 line_cardinality contributors.
+// This file is part of line_cardinality. Copyright © 2025 line_cardinality contributors.
 // line_cardinality is licensed under the GNU GPL v3.0 or any later version. See LICENSE file for full text.
 
 use hashbrown::HashTable;
